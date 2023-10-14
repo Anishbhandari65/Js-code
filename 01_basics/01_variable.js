bbcf393444e1b2024c0f82bc98b0e6c45daac563
Anishbhandari65 <129603490+Anishbhandari65@users.noreply.github.com>
2023-10-14 15:26:56 +0000

    let,const,var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId= 424242
+let accountEmail= "anishbhandari222@gmail.com"
+var accountPassword="Java"
+accountCity="Jaipur"
+
+//accountId= 2//
+
+accountEmail= "hdfc@gmail.com"
+accountPassword= "324443"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
